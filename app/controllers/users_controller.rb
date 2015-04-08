5aa0ae52da2101cda69aecabc3cef2c328322161
Tom Lee <tom_t_lee@yahoo.com>
2015-04-07 20:57:37 -0400

    Add new user controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      flash[:notice] = "You are now registered!"
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :timezone)
+    end
+
 end
