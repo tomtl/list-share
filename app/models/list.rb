4026256ec4048a34cfab7c587982d2238b395742
Tom Lee <tom_t_lee@yahoo.com>
2015-04-09 18:35:43 -0400

    Add percent complete to lists index view

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,11 +4,11 @@ class List < ActiveRecord::Base
   has_many :users, through: :list_users
 
   validates :name, presence: true
-  
-  def percent_complete(list)
-    if list.items.size > 0
-      total_items = list.items.size
-      completed_items = list.items.select{ |item| !item.completed_by.nil? }.size 
+
+  def percent_complete
+    if self.items.size > 0
+      total_items = self.items.size
+      completed_items = self.items.select{ |item| !item.completed_by.nil? }.size 
       100 * completed_items / total_items
     else
       0
